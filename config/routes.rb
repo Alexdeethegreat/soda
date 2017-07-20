3744515ca4f57c717571abc3daa6e97cef836483
Andrew Gottlieb <andrewgottlieb@Andrews-MacBook-Air.local>
2017-07-20 11:43:51 -0400

    httparty framework and routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :show]
-  resources :songs
+  resources :songs, only: [:create, :index, :new, :edit, :update]
   resources :artists
+
+  get "songs/search", to: "songs#search", as: :search
+
+  get "songs/:queryterm", to: "songs#query"
 
   get "/signup", to: "users#new", as: :signup
 
