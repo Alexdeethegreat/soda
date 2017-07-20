ece52ee143d13812cfefb6f451b72af719eaacc7
brpthomas <superbadpeter@Peters-MacBook-Air.local>
2017-07-20 17:18:42 -0400

    dev pull

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "sessions#new"
 
-<<<<<<< HEAD
-  resources :users, only: [:create, :show]
-  resources :songs, only: [:create, :index, :new, :edit, :update, :add]
-=======
   resources :users, only: [:create, :show, :edit, :update]
   resources :songs, only: [:create, :index, :new, :edit, :update]
->>>>>>> 85f89877401c9cd9a807f6dd08e2c54b2dffdc20
   resources :artists
 
   get "songs/favsong", to: "songs#add", as: :favsong
