9dd238cea6a539748ba26874c95abd092a5efa37
Andrew Gottlieb <andrewgottlieb@Andrews-MacBook-Air.local>
2017-07-21 10:41:56 -0400

    edit profile works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
    		if @user.save
 
      	  session[:user_id] = @user.id
-     	  redirect_to @user
+     	  redirect_to "/users/#{@user[:id]}/edit"
 
    		else
         p "user is being saved"
